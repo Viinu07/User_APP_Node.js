5eaa28585ac1d1d28f88d85b528f1c2a4f9bec3d
Viinu_07 <41299419+Viinu07@users.noreply.github.com>
2021-07-07 18:24:17 +0530

    Update controller.js

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -6,7 +6,7 @@ const utils = require('../common/utils');
 
 exports.create = (req, res) => {
   //Validating requests
-  if (!req.body) {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
     res.status(400).send({ message: 'Please Fill the details!' });
     return;
   }
@@ -29,8 +29,8 @@ exports.create = (req, res) => {
   user
     .save(user)
     .then((data) => {
-      /* res.send(data); */
-      res.redirect('/');
+      res.send(data);
+      //res.redirect('/');
     })
     .catch((err) => {
       res.status(500).send({
@@ -43,29 +43,43 @@ exports.create = (req, res) => {
 
 exports.find = (req, res) => {
   //Returning single user
-  if (req.query.id) {
-    const id = req.query.id;
+  //console.log(res.locals.loggedInUser)
+  const {offset,limit} = req.query
+  const sort = req.query.sort || "name"
+  const sortDirection = req.query.sortDirection || "asc"
+  if (req.params.id) {
+    const id = req.params.id;
     userDB
       .findById(id)
       .then((data) => {
         if (!data) {
           res.status(404).send({ message: 'Not found user id with id' + id });
         } else {
-          res.send(data);
+          if(res.locals.loggedInUser.role === "User" && res.locals.loggedInUser.id === id)
+          return res.send(data);
+          if(res.locals.loggedInUser.role === "Manager")
+          return res.send(data);
+          return res.status(403).send({message:"You are not allowed to access this id information " + id})
         }
       })
       .catch((err) => {
-        res.status(500).send({ message: 'Error retrieving user with id' + id });
+       return res.status(500).send({ message: 'Error retrieving user with id' + id });
       });
   } else {
+    //console.log(limit,offset)
     userDB
-      .find()
+      .find().sort({[sort]:sortDirection})
+      .skip(offset ? parseInt(offset) : 0).limit(limit ? parseInt(limit) : null)
       .then((user) => {
-        //console.log(user);
-        res.send(user);
+        console.log(res.locals.loggedInUser.id);
+        if(res.locals.loggedInUser.role === "User")
+        {res.send(user.filter(
+          (x) => x.id === res.locals.loggedInUser.id
+        ));}
+       else {res.send(user);}
       })
       .catch((err) => {
-        res.status(500).send({
+       return res.status(500).send({
           message:
             err.message || 'Error occured while retriving user information',
         });
@@ -76,46 +90,57 @@ exports.find = (req, res) => {
 //Updating an User
 
 exports.update = (req, res) => {
-  if (!req.body) {
+  //console.log(res.locals.loggedInUser)
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
     return res.status(400).send({ message: 'Update data should not be empty' });
   }
   const id = req.params.id;
+  if((res.locals.loggedInUser.role === "User" && res.locals.loggedInUser.id === id )|| res.locals.loggedInUser.role === "Manager")
+  {
   userDB
-    .findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-    .then((data) => {
-      if (!data) {
-        res
-          .status(404)
-          .send({ message: `Cannot update user with ${id}. User Not Found!` });
-      } else {
-        res.send(data);
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({ message: 'Error with Updated user information' });
-    });
+  .findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+  .then((data) => {
+    if (!data) {
+       res
+        .status(404)
+        .send({ message: `Cannot update user with ${id}. User Not Found!` });
+    } else {
+       res.send({message:`User update is successful for ${id}`});
+    }
+  })
+  .catch((err) => {
+    res.status(500).send({ message: 'User id unmatch' });
+  });
+  }else{
+    res.status(403).send({message:"You are not allowed to update this id information " + id})
+  }
 };
 
 //Deleting an User
 
 exports.delete = (req, res) => {
   const id = req.params.id;
-  userDB
+  if((res.locals.loggedInUser.role === "User" && res.locals.loggedInUser.id === id )|| res.locals.loggedInUser.role === "Manager")
+  {
+    userDB
     .findByIdAndDelete(id)
     .then((data) => {
       if (!data) {
-        res
+      return  res
           .status(404)
           .send({ message: `Cannot delete with id ${id}. Check your id` });
       } else {
-        res.send({
-          mesage: ' User was deleted successfully',
+      return  res.send({
+          mesage: 'User was deleted successfully',
         });
       }
     })
     .catch((err) => {
-      res.status(500).send({
+    return  res.status(500).send({
         message: 'Could not delete User with the id = ' + id,
       });
     });
+  }else{
+    res.status(403).send({message:"You are not allowed to delete this id information " + id})
+  }
 };
